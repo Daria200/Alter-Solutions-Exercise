734cb90e452735c94022c2f5a25e3e9aa7eee709
Daria Neznaeva <darianeznaeva@gmail.com>
2023-03-29 15:08:59 +0200

    docstring

--- banana_detector.py
+++ banana_detector.py
@@ -3,12 +3,19 @@ import logging
 
 
 def log_bananas():
+    """
+    Reads an input stream. Logs a minion to the banana.log file
+    when word banana is contained in the stream.
+    """
+
+    # Configure the file where the minion will be logged
     logging.basicConfig(
         filename="banana.log",
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
+    # Log the minion
     for line in sys.stdin:
         if "banana" in line.lower():
             logging.info(
