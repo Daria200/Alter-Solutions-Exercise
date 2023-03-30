1c5f0fe9028d90f1a83179d98889fe1017c69ab6
Daria Neznaeva <darianeznaeva@gmail.com>
2023-03-30 18:40:16 +0200

    renaming the banana_detector service

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo yum install python3 -y
 sudo cp service/banana_detector.py /usr/local/bin/
 
 # Copy the systemd service configuration file to /etc/systemd/system
-sudo cp banana_logger_service.service /etc/systemd/system/
+sudo cp banana_detector.service /etc/systemd/system/
 
 # Reload systemd configuration
 sudo systemctl daemon-reload
